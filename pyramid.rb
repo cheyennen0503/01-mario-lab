45ac82d26b22de2141ba15eccfc4b41564589739
cheyennen0503 <cheyennen0503@hstat.org>
2017-10-10 15:45:06 +0000

    create mario function

--- pyramid.rb
+++ pyramid.rb
@@ -1,1 +1,26 @@
-#write your code here
+
+#def pyramid
+   
+    #puts " "*4 + "#"*1 + " "*2 + "#"*1 
+    #puts " "*3 + "#"*2 + " "*2 + "#"*2
+    #puts " "*2 + "#"*3 + " "*2 + "#"*3
+   # puts " "*1 + "#"*4 + " "*2 + "#"*4
+   # puts " "*0 + "#"*5 + " "*2 + "#"*5
+   
+#end 
+
+#pyramid 
+
+def pyramid(height)
+    level = 1
+   height.times do
+    puts " "*(height-level) + "#"*level + " "*2 + "#"*level
+    level += 1
+  end 
+   
+end 
+
+# pyramid(7)
+puts "How tall do you want your pyramid to be?:"
+user_height = gets.chomp.to_i
+pyramid(user_height)
